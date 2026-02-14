ea7795a091ab2a9d189ccc765080285002a6d8a7
Davarius Bristow <davarius@email.uscb.edu>
2026-02-14 05:13:20 -0500

    Initialized Pseudocode

--- src/GCDApp.java
+++ src/GCDApp.java
@@ -4,4 +4,21 @@
  */
 public class GCDApp {
     
-}
+    public static void main(String[] args) {
+        
+        /* INITIALIZATION PHASE */
+        // Declare two integers
+        // Prompt user to enter first integer
+        // Prompt user to enter second integer
+        
+        /* PROCESSING PHASE */ 
+        // Call the integers
+        // Store the results in a variable
+        // Display the greatest common divisor
+        
+        /* TERMINATION PHASE */
+        
+        
+    } // end method main
+    
+} // end class GCDApp
